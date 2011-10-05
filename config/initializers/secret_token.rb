cbe83c544e98329da9293505998b1f77bc9cfbf4
Charlie Magee <charlie@signaldesign.net>
2011-10-05 14:25:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProveIt::Application.config.secret_token = 'e9ded2706e158efd82dbb600fcc47c91d0b6d19659f99168cb7dacc452ddd19568c2204254509cba969fc5b6417459bd4c64f57fc1cbf09e4695650e38acaeb3'
